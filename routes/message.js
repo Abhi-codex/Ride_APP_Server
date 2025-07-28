e341a139922c5fa5a98374abe7809d2070d0b14a
DhruvaSingh12 <singhdhruva45@gmail.com>
2025-07-28 23:05:48 +0530

    messaging

--- routes/message.js
+++ routes/message.js
@@ -1,12 +1,10 @@
 import express from "express";
 import auth from "../middleware/authentication.js";
 import { getMessages, sendMessage, deleteMessage, getUnreadCount, markMessagesRead, getChatList } from "../controllers/message.js";
-// List all chat partners/chats for the current user
-router.get("/chats", auth, getChatList);
 
 const router = express.Router();
-
-// GET /messages?appointmentId=... or ?otherUserId=...
+// List all chat partners/chats for the current user
+router.get("/chats", auth, getChatList);
 
 // Fetch chat history
 router.get("/", auth, getMessages);
